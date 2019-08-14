1e7f73e7dd3708c013bf203d0dbc444c3e98f531
Krstine BROOKS <270026659@bun2-206-04.acg.ac.nz>
2019-08-14 15:38:09 +1200

    tetrryry

---
+++ js/script.js
@@ -0,0 +1,11 @@
+console.log('my script is working');
+
+
+$(document).ready(function(){
+  $('.test').click(function(){
+    $('nav').toggleClass('active');
+  })
+//  $('#close').click(function(){
+  //  $('nav').hide();
+
+});
